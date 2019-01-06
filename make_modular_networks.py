b2d28f18ad3af2a441ea2ee7cc6dca2b18cc7ac1
tripdancer0916@gmail.com <ritalin202>
2019-01-07 01:01:33 +0900

    ネットワークの作り方を少し変更

--- make_modular_networks.py
+++ make_modular_networks.py
@@ -6,14 +6,24 @@ import matplotlib.pyplot as plt
 def make_modular_network(N, average_degree, community_number, mu):
     assert N % community_number == 0, 'N must be devisible by community_number'
     G = np.zeros((N, N))
+    size = N/community_number
     for i in range(N):
+        com_index = i//size
+        k_in_prev = 0
+        k_out_prev = 0
+        for j in range(int(size*com_index)):
+            if G[i][j] != 0:
+                k_out_prev += 1
+        for j in range(int(size*com_index), int(size*(com_index+1))):
+            if G[i][j] != 0:
+                k_in_prev += 1
         for j in range(i, N):
-            if j < (N/community_number)*(i//(N/community_number)+1):
-                if np.random.rand() < ((N-(i/N))/N)*average_degree*(1-mu)/(N/community_number):
+            if j < size*((i//size)+1):
+                if np.random.rand() < (average_degree*(1-mu)-k_in_prev)/(size-(i-(size*com_index))+1):
                     G[i][j] = np.random.randn()
                     G[j][i] = np.random.randn()
             else:
-                if np.random.rand() < ((N-(i/N))/N)*average_degree*(mu)/(N-(N/community_number)):
+                if np.random.rand() < (average_degree*(mu)-k_out_prev)/(N-(size*((i//size)+1))+1):
                     G[i][j] = np.random.randn()
                     G[j][i] = np.random.randn()
     return G
--- memory-capacity-task.py
+++ memory-capacity-task.py
@@ -50,16 +50,26 @@ def memory_capacity(L, buffer, data, output_data):
 def make_modular_network(N, average_degree, community_number, mu):
     assert N % community_number == 0, 'N must be devisible by community_number'
     G = np.zeros((N, N))
+    size = N/community_number
     for i in range(N):
+        com_index = i//size
+        k_in_prev = 0
+        k_out_prev = 0
+        for j in range(int(size*com_index)):
+            if G[i][j] != 0:
+                k_out_prev += 1
+        for j in range(int(size*com_index), int(size*(com_index+1))):
+            if G[i][j] != 0:
+                k_in_prev += 1
         for j in range(i, N):
-            if j < (N/community_number)*(i//(N/community_number)+1):
-                if np.random.rand() < ((N-(i/N))/N)*average_degree*(1-mu)/(N/community_number):
+            if j < size*((i//size)+1):
+                if np.random.rand() < (average_degree*(1-mu)-k_in_prev)/(size-(i-(size*com_index))+1):
                     G[i][j] = np.random.randn()
-                    G[j][i] = G[i][j]
+                    G[j][i] = np.random.randn()
             else:
-                if np.random.rand() < ((N-(i/N))/N)*average_degree*(mu)/(N-(N/community_number)):
+                if np.random.rand() < (average_degree*(mu)-k_out_prev)/(N-(size*((i//size)+1))+1):
                     G[i][j] = np.random.randn()
-                    G[j][i] = G[i][j]
+                    G[j][i] = np.random.randn()
     return G
 
 
@@ -239,7 +249,7 @@ if __name__ == '__main__':
     mu_list = np.arange(0, 0.80, 0.025)
     mc_list = []
     for mu in mu_list:
-        mc, _ = calculate_memory_capacity(mu=mu, r_sig=0.5, num_community=50)
+        mc, _ = calculate_memory_capacity(mu=mu, r_sig=0.5, num_community=20)
         print(mc, mu)
         mc_list.append(mc)
     mc_list = np.array(mc_list)
